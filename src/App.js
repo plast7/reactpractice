25b6f2ad79cef454aca7489dccda92586bdd0f34
plast7 <plast1206@naver.com>
2022-01-19 20:18:26 +0900

    배열 관리 / 마운트

--- src/App.js
+++ src/App.js
@@ -1,20 +1,80 @@
-import React, {useState} from 'react';
-import Counter from './Counter.js';
-import NumberTemplate from './NumberTemplate.js';
+import React, {useRef, useState} from 'react';
+import UserList from './UserList.js';
+import CreateUser from './CreateUser.js';
 
 function App() {
     const[inputs, setInputs] = useState({
-        number: 0,
-        count: 0,
+        username: '',
+        email: '',
     });
 
-    const {number, count} = inputs;
+    const {username, email} = inputs;
+    const onChange = (e) => {
+        const{ name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
+
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com',
+            active: true
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com',
+            active: false
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com',
+            active: false
+        }
+    ]);
+
+    const nextId = useRef(4);
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        };
+        setUsers(users.concat(user));
+
+        setInputs({
+            username: '',
+            email: '',
+        });
+        nextId.current += 1;
+    };
+
+    const onRemove = id => {
+        setUsers(users.filter(user => user.id !== id));
+    }
+
+    const onToggle = id => {
+        setUsers(
+            users.map(user =>
+                user.id === id ? {...user, active: !user.active} : user
+            )
+        );
+    };
 
     return (
         <>
-            <NumberTemplate number={number} count={count}/>
-            <Counter inputs={inputs} setInputs={setInputs}/>
-
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
         </>
     );
 }
