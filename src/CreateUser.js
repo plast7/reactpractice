c49c69a10c6df6909788ebee4727711a8d1f9df9
Chavo Kim <treeboy0384@naver.com>
2022-01-24 17:45:13 +0900

    :sparkles: Added styled CreateUser.js

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,22 +1,52 @@
 import React from 'react';
+import {Box, Button, makeStyles, TextField} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    input: {
+        fontSize: 20,
+        color: "red"
+    },
+    button: {
+        borderRadius: "40px",
+    }
+}))
 
 function CreateUser({ username, email, onChange, onCreate }) {
+    const classes = useStyles();
+
     return (
-        <div>
-            <input
+        <Box className={classes.root}>
+            <TextField
                 name="username"
-                placeholder="계정명"
+                label="계정명"
                 onChange={onChange}
                 value={username}
+                inputProps={{
+                    className: classes.input
+                }}
             />
-            <input
+            <TextField
+                inputProps={{
+                    className: classes.input
+                }}
                 name="email"
-                placeholder="이메일"
+                label="이메일"
                 onChange={onChange}
                 value={email}
             />
-            <button onClick={onCreate}>등록</button>
-        </div>
+            <Button
+                variant={"contained"}
+                onClick={onCreate}
+                className={classes.button}
+            >
+                등록
+            </Button>
+        </Box>
     );
 }
 
